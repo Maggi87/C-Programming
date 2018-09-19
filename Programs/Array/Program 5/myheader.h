6a763cdbe529205ccc470484cb4a13a69540614f
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-09-19 15:29:32 +0530

    Accept array from user accept one number from  and return last occurrence number of that number(using revers loop)
    input : brr[6] = {5, 4 , 6 , 5 ,8} Search No : 5
    	output : 3

--- Programs/Array/Program 5/myheader.h
+++ Programs/Array/Program 5/myheader.h
@@ -1,18 +1,18 @@
 /*
-	Accept array from user accept one number from  and return last occurrence number of that number(using forward loop) 
+	Accept array from user accept one number from  and return last occurrence number of that number(using revers loop) 
 	input : brr[6] = {5, 4 , 6 , 5 ,8} Search No : 5
 	output : 3
 	
 	Algorithm
 		START 
 			consider element from array as iNo
-			iterate from i to iNo	
+			iterate reverse from iNo to i-1	
 				if i is equal to  iNo
-						to store i value into iPos
+						break loop
 				increment counter by one
 			continue	
 
-			return iPos
+			return i
 		STOP
 */
 

0a162fefea8884029b7fb65547788685aa5078c6
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-09-19 16:06:55 +0530

    Accept array from user and print the addition of the number
    input : brr = {1,2,3,4,5}
    	output : 15

---
+++ Programs/Array/Program 6/myheader.h
@@ -0,0 +1,17 @@
+/*
+	Accept array from user and print the addition of the number
+	input : brr = {1,2,3,4,5}
+	output : 15
+
+	Algorithm
+
+	STAR
+		take on variable as sum assign value 0
+		Iterate loop i to n
+			take addition of sum and i and store addtion into sum
+			increament counter by one
+		continue
+	STOP
+*/
+
+int AdditionArray(int *,int);

eb0809467c83ac2ac60fdbf2bb97e6a75cfb420c
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-09-19 15:20:57 +0530

    Accept array from user accept one number from  and return last occurrence number of that number(using forward loop)
    input : brr[6] = {5, 4 , 6 , 5 ,8} Search No : 5
    output : 3

---
+++ Programs/Array/Program 4/myheader.h
@@ -0,0 +1,20 @@
+/*
+	Accept array from user accept one number from  and return last occurrence number of that number(using forward loop) 
+	input : brr[6] = {5, 4 , 6 , 5 ,8} Search No : 5
+	output : 3
+	
+	Algorithm
+		START 
+			consider element from array as iNo
+			iterate from i to iNo	
+				if i is equal to  iNo
+						to store i value into iPos
+				increment counter by one
+			continue	
+
+			return iPos
+		STOP
+*/
+
+#include<stdio.h>
+int DisplayLast(int *,int,int);

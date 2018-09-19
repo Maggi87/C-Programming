3fa88e1508128590ad2dc96bd0dfbea9febe3616
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-09-19 14:55:51 +0530

    Accept array from user display below input
    input brr = {1,5,3,4,2}
    	output
    	                 1
    			 1,2,3,4,5
    			 1,2,3
    			 1,2,3,4
    			 1,2

--- Programs/Array/Program 1/myheader.h
+++ Programs/Array/Program 1/myheader.h
@@ -1,13 +1,24 @@
 /*
 	Accept array from user display below input
 	
-	input brr = {1,2,3,4,5}
+	input brr = {1,5,3,4,2}
 	output 
 	         1
-			 1,2
+			 1,2,3,4,5
 			 1,2,3
 			 1,2,3,4
-			 1,2,3,4,5
+			 1,2			
+			 
+	Algorithm
+		START 
+			Iterate the array from 1st element to last element 
+				consider element from array as iNo
+				iterate from i to iNo	
+					print the value of counter
+					increment counter by one
+				continue
+			continue
+		STOP
 */
 
 void Display(int *,int);

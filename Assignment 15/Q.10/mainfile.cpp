252e77dc2bb9bb90fb769695d6a1b87af80f45e6
Datt87 <mauli.ingle@gmail.com>
2018-10-10 23:38:20 +0530

    pattern

---
+++ Assignment 15/Q.10/mainfile.cpp
@@ -0,0 +1,22 @@
+﻿/*
+    i/p 4 4
+    o/p
+    *       *       *       *
+    *       $       $       *
+    *       $       $       *
+    *       *       *       *
+*/
+#include<stdio.h>
+#include"myheader.h"
+
+int main()
+{
+	int iValue1 = 0, iValue2 = 0;
+
+	printf("Enter the Row and Coloum\n");
+	scanf("%d %d",&iValue1,&iValue2);
+
+	pattern(iValue1,iValue2);
+
+	return 0;
+}

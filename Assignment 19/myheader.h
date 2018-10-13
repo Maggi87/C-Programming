7fc1051c85af0e4de0872b99456ab98821da4f2b
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-10-13 19:23:17 +0530

    Assignment 19
    /*
    	Input : 4 4
    	Output :
    		A B C D
    		A 0 0 D
    		A 0 0 D
    		A B C D
    */
    void Pattern1(int,int);
    ----------------------------------------------------------------------
    /*
    	Input : 4 4
    	Output :
    		1 2 3 4
    		4 3 2 1
    		1 2 3 4
    		4 3 2 1
    */
    void Pattern2(int,int);
    
    ----------------------------------------------------------------------
    /*
    	Input : 4 4
    	Output :
    		1 1 1 1
    		2 2 2 2
    		3 3 3 3
    		4 4 4 4
    */
    void Pattern3(int,int);
    ----------------------------------------------------------------------
    /*
    	Input : 4 4
    	Output :
    	1 * 1 *
    	2 * 2 *
    	3 * 3 *
    	4 * 4 *
    */
    void Pattern4(int,int);
    ----------------------------------------------------------------------
    /*
    	Input : 4 4
    	Output :
    	$ @ @ @ @ @
    	* $ & & & @
    	* # $ & & @
    	* # # $ & @
    	* # # # $ @
    	* * * * * $
    */
    void Pattern5(int,int);

---
+++ Assignment 19/myheader.h
@@ -0,0 +1,52 @@
+/*
+	Input : 4 4 
+	Output : 
+		A B C D
+		A 0 0 D
+		A 0 0 D
+		A B C D
+*/
+void Pattern1(int,int);
+
+/*
+	Input : 4 4
+	Output : 
+		1 2 3 4
+		4 3 2 1
+		1 2 3 4
+		4 3 2 1
+*/
+void Pattern2(int,int);
+
+/*
+	Input : 4 4
+	Output :
+		1 1 1 1
+		2 2 2 2
+		3 3 3 3
+		4 4 4 4
+*/
+void Pattern3(int,int);
+
+/*
+	Input : 4 4
+	Output :
+	1 * 1 *
+	2 * 2 *
+	3 * 3 *
+	4 * 4 *
+*/
+void Pattern4(int,int);
+
+/*
+	Input : 4 4
+	Output :
+	$ @ @ @ @ @
+	* $ & & & @
+	* # $ & & @
+	* # # $ & @
+	* # # # $ @
+	* * * * * $
+*/
+void Pattern5(int,int);
+

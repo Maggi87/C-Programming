479e754a462e93b05b97aede92851f5a5d2cbece
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-10-15 13:31:17 +0530

    Assignment 21
    ///////////////////////////////////////////
    //
    // Input : 89 67 45 45 78
    // 5
    //
    // Output
    // 89 67 45 45 78
    // 00 67 45 45 78
    // 00 00 45 45 78
    // 00 00 00 45 78
    // 00 00 00 00 78
    //
    ///////////////////////////////////////////
    */
    void Pattern1(int *, int);
    ///////////////////////////////////////////
    //
    // Input : 89 67 45 11 78
    // 5
    //
    // Output
    // 89 00 45 00 78
    // 78 00 45 00 89
    // 89 00 45 00 78
    // 78 00 45 00 89
    // 89 00 45 00 78
    //
    ///////////////////////////////////////////
    void Pattern2(int *, int);
    
    ///////////////////////////////////////////
    //
    // Input : 89 67 45 11 78
    // 5
    // Output
    // 89 67 45 11 78
    // 89 67 45 11 00
    // 89 67 45 00 00
    // 89 67 00 00 00
    // 89 00 00 00 00
    ///////////////////////////////////////////
    void Pattern3(int *, int);
    ///////////////////////////////////////////
    //
    // Innput : 89 67 45 11
    // 4
    //
    // Output
    // 89 89 89 89
    // 67 67 67 67
    // 45 45 45 45
    // 11 11 11 11
    //
    ///////////////////////////////////////////
    void Pattern4(int *, int);
    ///////////////////////////////////////////
    //
    // Innput : 89 67 45 11
    // 4
    //
    // Output
    // 89 89 89 89
    // 67 67 67 67
    // 45 45 45 45
    // 11 11 11 11
    //
    ///////////////////////////////////////////
    void Pattern4(int *, int);
    ///////////////////////////////////////////
    //
    // Input : 89 67 45 11
    // 4
    //
    // Output
    // 89 00 89 00
    // 67 00 67 00
    // 45 00 45 00
    // 11 00 11 00
    //
    ///////////////////////////////////////////
    void Pattern5(int *, int);

---
+++ Assignment 21/mainfile.cpp
@@ -0,0 +1,64 @@
+﻿#include<stdio.h>
+#include"myheader.h"
+#include<malloc.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+
+int main()
+{
+	BOOL Running = TRUE;
+	int iChoice = 0, iSize = 0,i = 0;
+	int *arr = NULL;
+	printf("\n-- Marvellous Infosystems : Pattern Printing Application --\n\n");
+	while(Running)
+	{
+		printf("\nEnter size of Array : \t");
+		scanf("%d",&iSize);
+
+		arr = (int *)malloc(iSize * sizeof(int));
+		if(NULL == arr)
+		{
+			printf("Error in memory allocation\n");
+			return -1;
+		}
+
+		printf("\nEnter the Array Element\n");
+		for(i=0;i<iSize;i++)
+		{
+			scanf("%d",&arr[i]);
+		}
+		
+		printf("\nEnter the Choice\n");
+		scanf("%d",&iChoice);
+
+		switch(iChoice)
+		{
+			case 1:
+				Pattern1(arr,iSize);
+				break;
+			case 2:
+				Pattern2(arr,iSize);
+				break;
+			case 3:
+				Pattern3(arr,iSize);
+				break;
+			case 4:
+				Pattern4(arr,iSize);
+				break;
+			case 5:
+				Pattern5(arr,iSize);
+				break;			
+			case 0:
+				Running = FALSE;
+				break;
+			default:
+				printf("Wrong choice\n");
+				break;
+		}
+	}
+	printf("\nTerminating Pattern prinnting Application....\n");
+	free(arr);
+	return 0;
+}
